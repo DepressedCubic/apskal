832168571b0abe0113e152a7916cd90fef42552a
Carlos <carlos.martinezq03@gmail.com>
2024-06-10 04:15:01 +0200

    Simplify parsing

--- Parse.cs
+++ Parse.cs
@@ -1,3 +1,5 @@
+using System;
+
 class Lexer {
 
     public enum Token {
@@ -70,7 +72,13 @@ class Lexer {
                 token += text[position++];
             }
 
-            type = Token.Name;           
+            if (token == "rref") {
+                type = Token.Special;
+            }
+            else {
+                type = Token.Name;
+            }
+
             return token;
         }
         else {
@@ -83,93 +91,116 @@ class Lexer {
 class Parser {
 
     public enum ValueType {
-        Rational, Residue, RationalMatrix, ResidueMatrix
+        Rational, Residue, RationalMatrix, ResidueMatrix, None
     }
 
-    static string[] Functions = { "rref", "det" };
-    public abstract class Node {}
-
-    class Literal : Node {
-        string name;
-        ValueType type;
-
-        public Literal(string name, ValueType type) {
-            this.name = name;
-            this.type = type;
+    static bool IsPrime(uint n) {
+        for (uint d = 2; d * d <= n; ++d) {
+            if (n % d == 0) {
+                return false;
+            }
         }
+
+        return true;
     }
 
-    class Unary : Node {
-        string op;
-        Node child;
-
-        public Unary(string op, Node child) {
-            this.op = op;
-            this.child = child;
-        }
-    }
-
-    class Binary : Node {
-        string op;
-        Node left, right;
-
-        public Binary(string op, Node left, Node right) {
-            this.op = op;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    static void ParseCommand(
+    public static void ParseCommand(
         string command, 
         out string keyword, 
         out ValueType type, 
         out uint modulo, 
-        out string name) {
+        out string name,
+        out int height,
+        out int width) {
             if (command.Length == 0) {
                 throw new InvalidSyntaxException();
             }
 
             modulo = 0;
+            height = 0;
+            width = 0;
             
             Lexer l = new Lexer(command);
             keyword = l.Next(out Lexer.Token x);
 
+            if (keyword == "EXIT") {
+                type = ValueType.None;
+                name = "";
+                return;
+            }
+
             if (l.Next(out x) is string type_keyword) {
                 switch (type_keyword) {
                     case "Q":
+
                         type = ValueType.Rational;
                         break;
+
                     case "Z":
+
                         type = ValueType.Residue;
                         string m1 = l.Next(out x);
                         if (x == Lexer.Token.Literal) {
                             modulo = uint.Parse(m1);
+
+                            if (!IsPrime(modulo)) {
+                                throw new CompositeModuloException(modulo);
+                            }
+                            
                             break;
                         }
                         throw new InvalidSyntaxException();
+
                     case "Matrix":
+
                         l.Next(out x);
                         string field = l.Next(out x);
                         switch (field) {
                             case "Q":
+
                                 type = ValueType.RationalMatrix;
-                                l.Next(out x);
                                 break;
+
                             case "Z":
+
                                 type = ValueType.ResidueMatrix;
                                 string m2 = l.Next(out x);
                                 if (x == Lexer.Token.Literal) {
                                     modulo = uint.Parse(m2);
-                                    l.Next(out x);
+
+                                    if (!IsPrime(modulo)) {
+                                        throw new CompositeModuloException(modulo);
+                                    }
                                     break;
                                 }
                                 throw new InvalidSyntaxException();
+
                             default:
+
                                 throw new InvalidSyntaxException();
+
+                        }
+
+                        l.Next(out x);
+                        l.Next(out x);
+                        string h = l.Next(out Lexer.Token type_h);
+                        l.Next(out x);
+                        l.Next(out x);
+                        string w = l.Next(out Lexer.Token type_w);
+                        l.Next(out x);
+
+                        if ((type_h == Lexer.Token.Literal) && 
+                        (type_w == Lexer.Token.Literal)) {
+                            height = int.Parse(h);
+                            width = int.Parse(w);
+                        }
+                        else {
+                            throw new InvalidSyntaxException();
                         }
                         break;
+
                     default:
+
                         throw new InvalidSyntaxException();
                         
                 }
@@ -178,60 +209,14 @@ class Parser {
                 throw new InvalidSyntaxException();
             }
 
+            if (keyword == "EVAL") {
+                name = "";
+                return;
+            }
+
             name = l.Next(out x);
             if (x != Lexer.Token.Name) {
                 throw new InvalidSyntaxException();
             }
         }
-
-    static Node GenerateTree(ValueType type, string expression, uint modulo=0) {
-        Lexer l = new Lexer(expression);
-
-        Node Parse(ValueType current_type) {
-            string? token = l.Next(out Lexer.Token type_t);
-
-            if (type_t == Lexer.Token.Literal) {
-                string name = Runtime.StoreNumber(current_type, token, modulo);
-                return new Literal(name, current_type);
-            }
-
-            if (type_t == Lexer.Token.Name) {
-                switch (token) {
-                    case "det":
-                        if (current_type == ValueType.Rational) {
-                            Node c = Parse(ValueType.RationalMatrix);
-                            return new Unary("det", c);
-                        }
-                        else if (current_type == ValueType.Residue) {
-                            Node c = Parse(ValueType.ResidueMatrix);
-                            return new Unary("det", c);
-                        }
-                        else {
-                            throw new IncompatibleTypeException();
-                        }
-                    case "rref":
-                        Node child = Parse(current_type);
-                        return new Unary("rref", child);
-                    default:
-                        return new Literal(token, current_type);
-
-                }
-            }
-
-            if (token == "(") {
-                Node left = Parse(current_type);
-                string op = l.Next(out Lexer.Token x);
-                Node right = Parse(current_type);
-                if (l.Next(out x) != ")") {
-                    throw new InvalidSyntaxException();
-                }
-
-                return new Binary(op, left, right);
-            }
-
-            throw new InvalidSyntaxException();
-        }
-
-        return Parse(type);
-    }
 }
